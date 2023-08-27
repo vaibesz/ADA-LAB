a1b0310c9fcc97a54cfc04083658feedc8d478c9
vaibesz <117808219+vaibesz@users.noreply.github.com>
2023-08-27 13:57:33 +0530

    Create HeapSort.c

---
+++ HeapSort.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+void heapify(int arr[], int i, int size)
+{
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    int maxIdx = i;
+
+    if (left < size && arr[left] > arr[maxIdx])
+    {
+        maxIdx = left;
+    }
+    if (right < size && arr[right] > arr[maxIdx])
+    {
+        maxIdx = right;
+    }
+
+    if (maxIdx != i)
+    {
+        int temp = arr[maxIdx];
+        arr[maxIdx] = arr[i];
+        arr[i] = temp;
+        heapify(arr, maxIdx, size);
+    }
+}
+
+void heapSort(int arr[], int n)
+{
+	int i;
+    for (i = n / 2; i >= 0; i--)
+    {
+        heapify(arr, i, n);
+    }
+
+    for (i = n - 1; i >= 0; i--)
+    {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        heapify(arr, 0, i);
+    }
+}
+
+int main()
+{
+    int n, i;
+    printf("\nEnter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    heapSort(arr, n);
+
+    printf("Sorted Array:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
